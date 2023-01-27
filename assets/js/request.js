d7f5ea57970826ee28855a3ae2b528d6fcee501a
HogHordGit <hoghord@gmail.com>
2023-01-27 19:07:32 +0200

    make https method

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,4 +1,4 @@
 export const getData = (city) => 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=900c6ea1f9026a3abcf6c1620274ebbe&units=metric&lang=en`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=900c6ea1f9026a3abcf6c1620274ebbe&units=metric&lang=en`)
 	.then((res) => res.json());
 
